8ea3565c04c3f202b38ecb11ea1358b38b70ef86
Lisa Daly <10570708@mydbs.ie>
2022-08-26 16:35:18 +0100

    small layout - start auth

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,19 +1,39 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RouteConfigLoadEnd } from "@angular/router";
+import { of } from "rxjs";
+import { catchError, tap } from 'rxjs/operators'; 
 
+ 
 import { IUser } from "./usr.modl";
 
 @Injectable()
 export class AuthService{
 currentUser!:IUser;
 
+constructor(private httpClient: HttpClient)
+{}
     loginUser(userName: string, password: string){
-        this.currentUser = {
-            id:1,
-            userName: 'LDal',
-            access: 'user',
-            avatar: 'cat'
-        }
+
+        let loginInfo = { username: userName, password: password };
+
+        let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+
+
+        this.httpClient.post<IUser>('/api/login/', loginInfo, options)
+            .pipe(tap(data=> {
+                this.currentUser = data;
+            }))
+            .pipe(catchError(err => {
+                return of(false);
+            }))
+
+        // this.currentUser = {
+        //     id:1,
+        //     userName: 'LDal',
+        //     access: 'user',
+        //     avatar: 'cat'
+        // }
     }
 
     isAuthenticated() {
